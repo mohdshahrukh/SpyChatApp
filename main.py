06d43ae371633b43e4653d222a6dfc02e650b4b9
Mohammad Shahrukh <mohdshahrukh216@gmail.com>
2017-10-08 20:01:41 +0530

    created two variables spyname and spy salutation

---
+++ main.py
@@ -0,0 +1,12 @@
+print 'Hello'
+print 'let\'s get started'
+
+spy_name = raw_input('what is your name? ')
+
+print 'welcome '+ spy_name +' nice to meet you'
+
+spy_salutation =raw_input('what should we call you (mr. or ms.)?')
+
+
+spy_name = spy_salutation + ' ' + spy_name
+print 'welcome '+ spy_name
