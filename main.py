1f228c248563f02ac4916a4ed574264f4b956eb8
Mohammad Shahrukh <mohdshahrukh216@gmail.com>
2017-10-12 21:32:30 +0530

    added spy age and spyrating and completed spy authentication

--- main.py
+++ main.py
@@ -3,10 +3,26 @@ print 'let\'s get started'
 
 spy_name = raw_input('what is your name? ')
 
-print 'welcome '+ spy_name +' nice to meet you'
+if len(spy_name)>0:
+    print 'welcome '+ spy_name +' nice to meet you'
+    spy_salutation =raw_input('what should we call you (mr. or ms.)?')
+    spy_name = spy_salutation + ' ' + spy_name
+    print 'Alright ' + spy_name + ' i\'d like to know more about you'
+    spy_age = input( 'what is your age? ' )
+    if spy_age>12 and spy_age<50:
+        spy_rating = input ('what is your rating? ')
+        if spy_rating >4.5:
+            print 'very good spy'
+        elif spy_rating >3.5 and spy_rating<=4.5:
+            print 'good spy'
+        elif spy_rating >2.5 and spy_rating <=3.5:
+            print 'average spy'
+        else:
+            print 'bad spy'
+        print 'authentication complete. welcome ' + spy_name + ' age: ' + str(spy_age) + ' and rating : ' + str(spy_rating) + " Proud to have you onboard"
 
-spy_salutation =raw_input('what should we call you (mr. or ms.)?')
+    else:
+        print 'youre age is not eligible to be a spy'
 
-
-spy_name = spy_salutation + ' ' + spy_name
-print 'welcome '+ spy_name
+else:
+    print 'please enter your name'
